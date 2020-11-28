a8fbb6ae1ad28ec90f749988debed71834e7543c
LanKuDot <airlanser@gmail.com>
2020-11-28 23:00:48 +0800

    Create character controller example

---
+++ Assets/01-CharacterControler/Scripts/ByCharacterController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class ByCharacterController : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterController _characterController;
+    [SerializeField]
+    private float _speed = 8;
+
+    private Vector3 _direction;
+
+    private void Reset()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        var value = context.ReadValue<Vector2>();
+        _direction = new Vector3(value.x, 0, value.y);
+    }
+
+    private void FixedUpdate()
+    {
+        _characterController.SimpleMove(_speed * _direction);
+        // or
+        // _characterController.Move(_speed * Time.deltaTime * _direction);
+    }
+}
