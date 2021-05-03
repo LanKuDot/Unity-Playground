05e8679aba67cbc003398927a9dc7f7a3eb7566e
LanKuDot <airlanser@gmail.com>
2021-05-03 22:43:45 +0800

    Control the looking direction of the player

--- Assets/01-CharacterControler/Scripts/ByCharacterController.cs
+++ Assets/01-CharacterControler/Scripts/ByCharacterController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(CharacterController))]
@@ -10,10 +11,25 @@ public class ByCharacterController : MonoBehaviour
     private float _speed = 8;
 
     private Vector3 _direction;
+    private Func<Vector2, float> _lookAction;
+    private float _lookingDeg;
 
     private void Reset()
     {
         _characterController = GetComponent<CharacterController>();
+    }
+
+    private void Awake()
+    {
+        _lookAction = LookAtPointer;
+    }
+
+    public void OnInputDeviceChanged(PlayerInput input)
+    {
+        if (input.currentControlScheme.Equals("Gamepad"))
+            _lookAction = LookByStick;
+        else
+            _lookAction = LookAtPointer;
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -22,10 +38,33 @@ public class ByCharacterController : MonoBehaviour
         _direction = new Vector3(value.x, 0, value.y);
     }
 
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        _lookingDeg = _lookAction(context.ReadValue<Vector2>());
+    }
+
+    private float LookAtPointer(Vector2 pointerPos)
+    {
+        var playerScreenPos =
+            (Vector2) Camera.main.WorldToScreenPoint(transform.position);
+
+        return -Vector2.SignedAngle(Vector2.up, pointerPos - playerScreenPos);
+    }
+
+    private float LookByStick(Vector2 direction)
+    {
+        if (direction.magnitude < 0.1)
+            return _lookingDeg;
+
+        return -Vector2.SignedAngle(Vector2.up, direction);
+    }
+
     private void FixedUpdate()
     {
         _characterController.SimpleMove(_speed * _direction);
         // or
         // _characterController.Move(_speed * Time.deltaTime * _direction);
+
+        transform.rotation = Quaternion.Euler(0, _lookingDeg, 0);
     }
 }
